0d18ad2fbc42547d908085b966455c8b71e5feab
yuqiyang <yqiyang@ucdavis.edu>
2019-09-28 11:45:15 -0700

    No14

---
+++ 9PalindromeNumber.js
@@ -0,0 +1,16 @@
+// 9. Palindrome Number
+// Determine whether an integer is a palindrome.
+// An integer is a palindrome when it reads the
+// same backward as forward.
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 ||(x>0 && x%10==0)) return false;
+        int rev = 0;
+        while(x > rev){
+          rev = rev*10 + x%10;
+          x = x/10;
+        }
+        return (x==rev||(x！=rev && x == rev/10));
+    }
+}
